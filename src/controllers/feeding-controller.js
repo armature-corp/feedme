032ab70089ea46f0727b9d40ecb23a8717dbf8f3
James Simonds <james.simonds@armaturecorp.com>
2017-11-02 20:31:41 -0400

    more

--- src/controllers/feeding-controller.js
+++ src/controllers/feeding-controller.js
@@ -1,36 +1,34 @@
 var Feeding = require('../lib/models/feeding');
 var FeedingResult = require('../lib/models/feeding-result');
 
-export class FeedingController {
-    constructor() {
-        this.votes = [];
-        this.feeding = null;
-    }
-
-    start() {
-        this.feeding = new Feeding();
-        this.feeding.start = new Date();
-        return this.feedingId;
-    }
-    
-    list() {
-        return [
-            'Pei Wei',
-            'Urban BBQ',
-            'Subway',
-            'Thai Thai',
-            'Taco Bell'
-        ];
-    }
-    
-    vote(keyword) {
-        this.votes.push(keyword);
-    }
-    
-    end() {
-        let result = new FeedingResult();
-        result.feeding = this.feeding;
-        result.votes = this.votes;
-        return result;
-    }
+FeedingController = function() {
+    this.votes = [];
+    this.feeding = null;
 }
+
+FeedingController.prototype.start = function() {
+    this.feeding = new Feeding();
+    this.feeding.start = new Date();
+    return this.feedingId;
+};
+
+FeedingController.prototype.list = function() {
+    return [
+        'Pei Wei',
+        'Urban BBQ',
+        'Subway',
+        'Thai Thai',
+        'Taco Bell'
+    ];
+};
+
+FeedingController.prototype.vote = function(keyword) {
+    this.votes.push(keyword);
+};
+
+FeedingController.prototype.end = function() {
+    var result = new FeedingResult();
+    result.feeding = this.feeding;
+    result.votes = this.votes;
+    return result;
+};
