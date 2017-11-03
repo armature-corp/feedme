8b0115b4001109c66042d366cb46412207844d3e
James Simonds <james.simonds@armaturecorp.com>
2017-11-02 20:18:09 -0400

    start method

--- src/controllers/feeding-controller.js
+++ src/controllers/feeding-controller.js
@@ -1,6 +1,18 @@
+import { Feeding } from 'lib/models/feeding';
+
 export class FeedingController {
+    constructor() {
+        this.state = {
+            currentFeedingId: 1,
+            feedings: []
+        };
+    }
+
     start() {
-        // TODO
+        let instance = new Feeding();
+        instance.feedingId = this.state.currentInstanceId++;
+        this.state.feedings.push(instance);
+        return instance.feedingId;
     }
     
     list() {
